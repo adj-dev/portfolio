32ad5a6ac501904f2aa467bb0fbc4dc388b9f84f
adj-dev <andrew.johnson2073@gmail.com>
2019-07-13 16:41:13 -0500

    Added splash

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import SplashPage from './components/SplashPage'
+import Main from './components/Main'
 import PageNotFound from './components/PageNotFound'
 
 import './assets/css/index.css'
@@ -8,7 +8,7 @@ import './assets/css/index.css'
 const app = () => (
   <Router>
     <Switch>
-      <Route exact path='/' component={SplashPage} />
+      <Route exact path='/' component={Main} />
       <Route component={PageNotFound} />
     </Switch>
   </Router>
---
+++ src/components/Main/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Splash from './Splash'
+import Projects from './Projects'
+import './style.css'
+
+const Main = () => {
+  return (
+    <>
+      <Splash />
+      <Projects />
+    </>
+  )
+}
+
+export default Main
