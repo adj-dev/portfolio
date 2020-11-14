fc975887354eea69bf87f369b98aadbdfa52c6cf
adj-dev <andrew.johnson2073@gmail.com>
2020-11-14 10:27:11 -0600

    added correct import statements for project screenshots

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,10 +1,15 @@
+import protaImg from '../images/prota.png'
+import enscribeImg from '../images/escribe.png'
+import liriImg from '../images/liri.png'
+import gifmeImg from '../images/gifme.png'
+
 export const projects = [
   {
     id: 1,
     title: 'Prota',
     brief: 'A project management tool for GitHub developers - invite collaborators, develop projects, view sprints, and take on tasks with this agile-inspired service.',
     synopsis: 'A true MERN application that uses OAuth for authentication. My involvement with this ongoing project is as a lead React developer. In addition to writing many of the components I also manage and design the UI. I utilize Reacts hooks in many of my functional compenents, this improves performance while allowing for local state management.',
-    image: require('../images/prota.png'),
+    image: protaImg,
     repoLink: 'https://github.com/adj-dev/prota',
     deployment: {
       link: 'https://majestic-mesa-verde-10359.herokuapp.com',
@@ -16,7 +21,7 @@ export const projects = [
     title: 'Enscribe',
     brief: 'Enscribe is a tool for tutors to share lesson notes with their students.',
     synopsis: 'Built using Handlebars on the front-end and MySQL on the back-end. This was a fun project that allowed me to become familiar with using Sequelize to communicate with a MySQL database. My role in this project was in creating models for the database, writing API routes, and designing the UI.',
-    image: require('../images/escribe.png'),
+    image: enscribeImg,
     repoLink: 'https://github.com/adj-dev/escribe',
     deployment: {
       link: 'https://pure-wave-91989.herokuapp.com',
@@ -28,7 +33,7 @@ export const projects = [
     title: 'GIFme',
     brief: 'Search through millions of GIFs',
     synopsis: 'A simple project that I built using HTML, CSS, and JavaScript. It uses jQuery to dynamically change the DOM and Axios for making calls to the GIPHY API.',
-    image: require('../images/gifme.png'),
+    image: gifmeImg,
     repoLink: 'https://github.com/adj-dev/GIFme',
     deployment: {
       link: 'https://adj-dev.github.io/GIFme/',
@@ -40,7 +45,7 @@ export const projects = [
     title: 'LIRI',
     brief: 'Find songs from Spotify, shows from Bands In Town, and movies from OMDB',
     synopsis: 'A fun little project that runs entirely in the terminal. It is written exclusively in JavaScript and runs in a Node environment. Check out the repo for examples of how it works (I had a lot of fun with this one).',
-    image: require('../images/liri.png'),
+    image: liriImg,
     repoLink: 'https://github.com/adj-dev/liri',
     deployment: null
   },
